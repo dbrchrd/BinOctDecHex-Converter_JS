f34e4597a271ebf6ecc6f95ba4962462190af197
dbrchrd <83125728+dbrchrd@users.noreply.github.com>
2022-02-16 20:58:54 +0100

    Update index.js
    importing func & configuration of express

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 // Modules
 const express = require('express');
 const bodyParser = require('body-parser');
+// Functions
+const { convert } = require('./conversion');
+// Configuration
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
