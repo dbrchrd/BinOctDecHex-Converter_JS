7f1162bf936cad248e6f29fd622f626184861d8c
dbrchrd <83125728+dbrchrd@users.noreply.github.com>
2022-02-25 21:54:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
   console.log(req.query);
+  let converted = convert(req.query.value, req.query.from, req.query.to)
+  console.log(converted);
+  if (!converted.error) {
+    res.send(`Value: ${converted.v} From: ${converted.from} => To: ${converted.to} = ${converted.res}`);
+  } else {
+    res.send(`Error: ${converted.error}`);
+  }
 });
 
 app.listen(6060);
