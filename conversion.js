30850a00e0e0fb636f34fd009002d96ad97cad08
dbrchrd <83125728+dbrchrd@users.noreply.github.com>
2022-04-03 19:41:44 +0200

    Update conversion.js
    condition

--- conversion.js
+++ conversion.js
@@ -1,5 +1,11 @@
 module.exports.convert = (v, from, to) => {
   v = parse10(v); from = parse10(from); to = parse10(to);
+  if (
+      ( from == 2 || (from == 8) || (from == 10) || (from == 16) )
+      &&
+      ( (to == 2) || (to == 8) || (to == 10) || (to == 16) )
+    )
+    return { res: from2(from, to, v), v, from, to };
 }
 const from2 = (from, to, v) => {
   return parseInt(v, from).toString(to);
