244adf1756d607392d245bf3151eec6d1a6abb8d
dbrchrd <83125728+dbrchrd@users.noreply.github.com>
2022-04-03 19:47:03 +0200

    Update conversion.js
    return the errror

--- conversion.js
+++ conversion.js
@@ -5,6 +5,7 @@ module.exports.convert = (v, from, to) => {
   else {
     let error = `The template should be : http://domain.extension/?value=[NUMBER]&from=[2/8/10/16]&to=[2/8/10/16]\nPlease enter: 10 for decimal, 16 for hexadecimal, 8 for octal and 2 for binary.\nPlease enter a different value for "from" and "to".`
     console.error( error );
+    return { error };
   }
 }
 const from2 = (from, to, v) => {
