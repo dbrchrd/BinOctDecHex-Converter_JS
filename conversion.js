2033d0c43296710d979be7242a5e94a3f5b86fac
dbrchrd <83125728+dbrchrd@users.noreply.github.com>
2022-04-04 09:06:22 +0200

    Update conversion.js
    add req

--- conversion.js
+++ conversion.js
@@ -1,4 +1,4 @@
-module.exports.convert = (v, from, to) => {
+module.exports.convert = (req, v, from, to) => {
   v = parse10(v); from = parse10(from); to = parse10(to);
   if ( ( ( from == 2 || (from == 8) || (from == 10) || (from == 16) ) && ( (to == 2) || (to == 8) || (to == 10) || (to == 16) ) ) && (from != to) )
     return { res: from2(from, to, v), v, from, to };
