eba0bc55d25d840a69dd0c82c20fd3de621d4140
dbrchrd <83125728+dbrchrd@users.noreply.github.com>
2022-04-26 21:52:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
 // Modules
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 // Functions
-const { convert } = require('./conversion');
+const { convert } = require("./conversion");
 // Configuration
 const app = express();
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   console.log(req.query);
   console.log(req.route.path);
-  let converted = convert(req, req.query.value, req.query.from, req.query.to)
+  let converted = convert(req, req.query.value, req.query.from, req.query.to);
   console.log(converted);
   if (!converted.error) {
-    res.send(`${converted.v}<sub>(${converted.from})</sub> = ${converted.res}<sub>(${converted.to})</sub>`);
+    res.send(
+      `${converted.v}<sub>(${converted.from})</sub> = ${converted.res}<sub>(${converted.to})</sub>`
+    );
   } else {
     res.send(`Error: ${converted.error}`);
   }
 });
 
 // Listening on port 6060
-app.listen(6060);
+app.listen(6060, "127.0.0.1"); // Development
+// app.listen(6060, "0.0.0.0"); // Production
