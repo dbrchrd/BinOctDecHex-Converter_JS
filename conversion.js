e84382465e3deccaf19d8516960bee90dfb183f0
dbrchrd <83125728+dbrchrd@users.noreply.github.com>
2022-05-27 13:31:15 +0200

    Update conversion.js

--- conversion.js
+++ conversion.js
@@ -1,16 +1,16 @@
 module.exports.convert = (req, v, from, to) => {
   v = parse10(v);
-  from = parse10(from);
-  to = parse10(to);
+  // from = parse10(from);
+  // to = parse10(to);
   try {
     if (
-      (from != 2 || from != 8 || from != 10 || from != 16) &&
-      (to != 2 || to != 8 || to != 10 || to != 16)
+      (from != "2" || from != "8" || from != "10" || from != "16") &&
+      (to != "2" || to != "8" || to != "10" || to != "16")
     )
       return { res: from2(from, to, v), v, from, to };
   } catch (error) {
     console.error(error);
-    return `The template should be : http://domain.extension/[NUMBER_TO_CONVERT]/[2/8/10/16]/[2/8/10/16]\nPlease enter: 10 for decimal, 16 for hexadecimal, 8 for octal and 2 for binary.\nPlease enter a different value for "from" and "to".`;
+    return `The template should be : http://domain.extension/[2/8/10/16]/[2/8/10/16]/[NUMBER_TO_CONVERT]\nPlease enter: 10 for decimal, 16 for hexadecimal, 8 for octal and 2 for binary.\nPlease enter a different value for "from" and "to".`;
   }
 };
 const from2 = (from, to, v) => {
