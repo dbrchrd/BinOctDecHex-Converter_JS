1f880725c7b4c816d2e670017180b7a15ffd232a
dbrchrd <83125728+dbrchrd@users.noreply.github.com>
2022-06-17 09:56:03 +0200

    Update index.js
    Adding bases infos

--- index.js
+++ index.js
@@ -32,7 +32,13 @@ app.get("/:from/:to/:value", (req, res) => {
 
 app.get("*", (req, res) => {
   res.status(500).json({
-    error: "The conversion url template is : DOMAIN.EXTENTION/FORMAT_OF_UNCONVERTED_VALUE/FORMAT_OF_CONVERTED_VALUE/VALUE_TO_CONVERT : e.g. DOMAIN.EXTENTION/10/16/1515 -> convert 1515 in base 10 (dec) to base 16 (hex) = 5eb"
+    error: "The conversion url template is : DOMAIN.EXTENTION/FORMAT_OF_UNCONVERTED_VALUE/FORMAT_OF_CONVERTED_VALUE/VALUE_TO_CONVERT : e.g. DOMAIN.EXTENTION/10/16/1515 -> convert 1515 in base 10 (dec) to base 16 (hex) = 5eb",
+    bases: {
+      "2": "binary",
+      "8": "octal",
+      "10": "decimal",
+      "16": "hexadecimal"
+    }
   })
 });
 
